60319f7ce5adf1a33edc42d84ad5d5e47b27592d
FRVR-JAE <tjoloyede9@gmail.com>
2023-09-21 04:42:59 -0700

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
-
+import {Button} from '@/components/ui/button'
 
 export default function Home() {
   return (
-   <p>how far</p>
+   <p>
+    <div className='p-4'>
+         <Button>Click Me</Button>
+    </div>
+    </p>
   )
 }
